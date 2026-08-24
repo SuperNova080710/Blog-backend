7f4c1238c41deaaaeb38caadfac21ba70e3ec761
Odyz <lionoh00@gmail.com>
2026-08-24 11:07:03 +0900

    chore(config): 환경변수 기본 구성 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from "@nestjs/common";
 import { HealthCheckModule } from "./modules/health-check/health-check.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-    imports: [HealthCheckModule],
+    imports: [ConfigModule.forRoot({
+        isGlobal: true,
+        }), 
+        HealthCheckModule,
+    ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
-    await app.listen(3000);
+    const port = process.env.PORT || 3000;
+
+    await app.listen(port);
     console.log('Server is running on http://localhost:3000');
 }
 
