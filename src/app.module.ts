25660b81075ba38a77a90942ac87786a85968f2b
Odyz <lionoh00@gmail.com>
2026-08-26 15:59:29 +0900

    feat: NestJS와 PostgreSQL TypeORM 연결 구성
    TypeORM 및 PostgreSQL 연동 패키지 설치
    DB 환경변수 Joi validation 추가
    TypeOrmModule을 통한 PostgreSQL 연결 설정
    TypeORM synchronize 비활성화
    NestJS 애플리케이션에서 PostgreSQL 연결 확인

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
 import { HealthCheckModule } from "./modules/health-check/health-check.module";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService} from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import * as Joi from 'joi';
 
 @Module({
@@ -15,9 +16,44 @@ import * as Joi from 'joi';
             PORT: Joi.number()
             .port()
             .default(3000),
+            
+            DB_HOST: Joi.string()
+            .required(),
+
+            DB_PORT: Joi.number()
+            .port()
+            .default(5432),
+
+            DB_USERNAME: Joi.string()
+            .required(),
+
+            DB_PASSWORD: Joi.string()
+            .required(),
+
+            DB_DATABASE: Joi.string()
+            .required(),
         })
 
         }), 
+
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+
+            useFactory: (configService: ConfigService) => ({
+                type: "postgres",
+                host: configService.get<string>("DB_HOST"),
+                port: configService.get<number>("DB_PORT"),
+                username: configService.get<string>("DB_USERNAME"),
+                password: configService.get<string>("DB_PASSWORD"),
+                database: configService.get<string>("DB_DATABASE"),
+
+                autoLoadEntities: true,
+
+                // Migration으로 스키마를 관리하므로 false
+                synchronize: false,
+            }),
+        }),
+
         HealthCheckModule,
     ],
 })
