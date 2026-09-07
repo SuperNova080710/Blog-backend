ee536f5a1731a71b2a7a3b773fb7949b33654765
Odyz <lionoh00@gmail.com>
2026-09-07 12:15:30 +0900

    feat(auth): 회원가입 API 구현
    - SignupDto를 통한 이메일 및 비밀번호 유효성 검사
    - bcrypt를 사용한 비밀번호 암호화
    - 이메일 중복 검사 및 ConflictException 처리
    - 회원가입 성공 시 사용자 ID와 이메일 반환
    - 회원가입 API 예외 및 Validation 테스트

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable } from "@nestjs/common";
+import * as bcrypt from "bcrypt";
+import { UsersService } from "../users/users.service";
+import { SignupDto } from "./dto/signup.dto";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly usersService: UsersService,
+    ) {}
+
+    async signup(signupDto: SignupDto) {
+        const { email, password } = signupDto;
+
+        const existingUser = await this.usersService.findByEmail(email);
+
+        if( existingUser ) {
+            throw new ConflictException("This email is already in use.");
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await this.usersService.create(
+            email,
+            hashedPassword,
+        );
+
+        return {
+            id: user.id,
+            email: user.email,
+        };
+    }
+}
